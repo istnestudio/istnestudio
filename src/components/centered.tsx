44c82bbad012a323aa590610adadbb82fd9bef00
BlacKlyExactly <supreme24d@gmail.com>
2026-01-21 10:09:58 +0100

    chore(app): Simplify centered component

--- src/components/centered.tsx
+++ src/components/centered.tsx
@@ -1,29 +1,12 @@
-import {
-  Children,
-  cloneElement,
-  isValidElement,
-  PropsWithChildren,
-  ReactElement,
-} from "react";
+import { cn } from "@/utils/cn";
+import { cloneElement, ReactElement } from "react";
 
-type WithClassNameAndRef = {
-  className?: string;
-  ref?: React.Ref<HTMLDivElement>;
-  forwardedRef?: React.Ref<HTMLDivElement>;
-};
-
-export const Centered = ({ children }: PropsWithChildren) => {
-  const childrenArr = Children.toArray(children);
-
-  if (childrenArr.length > 0 && isValidElement(childrenArr[0])) {
-    const first = childrenArr[0] as ReactElement<WithClassNameAndRef>;
-    const existingRef = first.props.ref || first.props.forwardedRef;
-
-    childrenArr[0] = cloneElement(first, {
-      className: `${first.props.className || ""} lg:w-[calc(33%-12px)]`,
-      ref: existingRef,
-    });
-  }
-
-  return childrenArr;
+export const Centered = ({
+  children,
+}: {
+  children: ReactElement<{ className?: string }>;
+}) => {
+  return cloneElement(children, {
+    className: cn(children.props.className, "lg:w-[calc(33%-12px)]"),
+  });
 };
